afc7cdf494aeeac49b0fa8c62417692d1117da2c
sasimiseo <126683080+sasimiseo@users.noreply.github.com>
2025-10-11 15:41:32 +0900

    Add files via upload

---
+++ src/test/java/com/example/demo1010/Demo1010ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo1010;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1010ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
