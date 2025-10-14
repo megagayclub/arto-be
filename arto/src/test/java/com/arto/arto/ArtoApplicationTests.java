73cd31454969540a9ab6c808bcfa94fdcd0a6e71
sewoo-hub <80295333+sewoo-hub@users.noreply.github.com>
2025-10-14 21:26:25 +0900

    1
    1

---
+++ arto/src/test/java/com/arto/arto/ArtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arto.arto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
