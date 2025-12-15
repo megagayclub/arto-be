e09ead0dd8f86943a79eef5d4aa02d1fb54ce69f
Userskin1 <seunghwanj56@gmail.com>
2025-12-15 10:50:17 +0900

    Fix: Update JWT secret to ensure minimum 256-bit length

--- src/main/java/com/arto/arto/test/testController.java
+++ src/main/java/com/arto/arto/test/testController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class testController {
     @GetMapping("/health")
     public String healthCheck() {
-        return "Let's Go";
+        return "LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL";
     }
 }
